46f0ea12f6a98ca37de0eb61e7f5e9bbf5b53344
Barkın Köroğlu <barkin_45_99@hotmail.com>
2024-08-12 14:00:05 +0300

    Menu section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services";
 import Selection from "./components/Selection";
+import Menu from "./components/Menu";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <About />
       <Services />
       <Selection />
+      <Menu />
     </div>
   );
 };
